d457439d4afc777802e1a7c92afcf095ed30e6a0
jgw3352 <jgw0023@likelion.org>
2021-07-06 11:07:30 +0900

    7/6 4ban class commit 1

--- projects/views.py
+++ projects/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def hello_world(request):
-    return render(request, 'base.html')
+    return render(request, 'projects/hello_world.html')
 
