8d99cc49c55619a0f533de4d1ad597050cc99885
jgw3352 <jgw0023@likelion.org>
2021-07-09 12:39:55 +0900

    7/9 4ban class commit 4

--- projects/views.py
+++ projects/views.py
@@ -3,12 +3,20 @@ from django.shortcuts import render
 
 
 # Create your views here.
+from projects.models import NewModel
+
+
 def hello_world(request):
     if request.method == "POST":
+
+        temp = request.POST.get('input_text')
+
+        new_model = NewModel()
+        new_model.text = temp
+        new_model.save()
+
         return render(request, 'projects/hello_world.html',
-                      context={'text': 'POST METHOD'})
+                      context={'new_model': new_model})
     else:
         return render(request, 'projects/hello_world.html',
                       context={'text': 'GET METHOD'})
-
-
