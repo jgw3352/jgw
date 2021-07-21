addc3b0a92c7699a65c46f24d3262b23bfefcc4c
jgw3352 <jgw0023@likelion.org>
2021-07-21 16:50:50 +0900

    7/21 4ban class commit 1

--- projects/views.py
+++ projects/views.py
@@ -8,28 +8,29 @@ from django.shortcuts import render
 # Create your views here.
 from django.urls import reverse, reverse_lazy
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
-
+from projects.forms import AccountCreationForm
 from projects.models import NewModel
 
 
 def hello_world(request):
-    if request.method == "POST":
+    if request.user.is_authenticated:
+        if request.method == "POST":
 
-        temp = request.POST.get('input_text')
+            temp = request.POST.get('input_text')
 
-        new_model = NewModel()
-        new_model.text = temp
-        new_model.save()
+            new_model = NewModel()
+            new_model.text = temp
+            new_model.save()
 
 
 
-        return HttpResponseRedirect(reverse('projects:hello_world'))
+            return HttpResponseRedirect(reverse('projects:hello_world'))
+        else:
+            data_list = NewModel.objects.all()
+            return render(request, 'projects/hello_world.html',
+                         context={'data_list': data_list})
     else:
-        data_list = NewModel.objects.all()
-        return render(request, 'projects/hello_world.html',
-                      context={'data_list': data_list})
-
-
+        return HttpResponseRedirect(reverse('projects:login'))
 
 
 
@@ -40,6 +41,8 @@ class AccountCreateView(CreateView):
     template_name = 'projects/create.html'
 
 
+
+
 class AccountDetailView(DetailView):
     model = User
     context_object_name = 'target_user'
@@ -48,13 +51,43 @@ class AccountDetailView(DetailView):
 
 class AccountUpdateView(UpdateView):
     model = User
-    form_class = UserCreationForm
+    form_class = AccountCreationForm
     context_object_name = 'target_user'
     success_url = reverse_lazy('projects:hello_world')
     template_name = 'projects/update.html'
+
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return super().get(request, *args , **kwargs)
+        else:
+            return HttpResponseRedirect(reverse('projects:login'))
+
+    def post(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return super().get(request, *args, **kwargs)
+        else:
+            return HttpResponseRedirect(reverse('projects:login'))
+
+
+
 
 class AccountDeleteView(DeleteView):
     model = User
     context_object_name = 'target_user'
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'projects/delete.html'
+
+
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return super().get(request, *args , **kwargs)
+        else:
+            return HttpResponseRedirect(reverse('projects:login'))
+
+    def post(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return super().get(request, *args, **kwargs)
+        else:
+            return HttpResponseRedirect(reverse('projects:login'))
