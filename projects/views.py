3f1e5e18fdce28dc11d396fdd520c2884c1a2a6b
jgw3352 <jgw0023@likelion.org>
2021-08-18 18:13:10 +0900

    8/18 4ban class commit 1

--- projects/views.py
+++ projects/views.py
@@ -8,7 +8,9 @@ from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
+from django.views.generic.list import MultipleObjectMixin
 
+from articleapp.models import Article
 from projects.decorators import account_ownership_required
 from projects.forms import AccountCreationForm
 from projects.models import NewModel
@@ -36,11 +38,17 @@ class AccountCreateView(CreateView):
     template_name = 'projects/create.html'
 
 
-class AccountDetailView(DetailView):
+class AccountDetailView(DetailView, MultipleObjectMixin):
     model = User
     context_object_name = 'target_user'
     template_name = 'projects/detail.html'
 
+    paginate_by = 20
+
+    def get_context_data(self, **kwargs):
+        article_list = Article.objects.filter(writer=self.object)
+        return super().get_context_data(object_list=article_list,
+                                        **kwargs)
 
 has_ownership = [login_required, account_ownership_required]
 
